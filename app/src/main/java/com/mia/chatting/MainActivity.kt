809ea0dedaecf1e079c14edc7b0ecd8f4319c7e2
Mia <mikyung.yong@poliot.co.kr>
2023-07-10 17:16:51 +0900

    타이틀삭제, 상태바 흰색, 다크모드 비활성화

--- app/src/main/java/com/mia/chatting/MainActivity.kt
+++ app/src/main/java/com/mia/chatting/MainActivity.kt
@@ -2,10 +2,12 @@ package com.mia.chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // 다크모드 비활성화
     }
 }
