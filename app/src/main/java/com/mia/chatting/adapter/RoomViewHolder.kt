1899433399246965dda414d878bedb22e0c98319
Mia <mikyung.yong@poliot.co.kr>
2023-07-25 09:16:49 +0900

    채팅방 목록에서 상대방이름, 날짜, 내용 표출 추가

--- app/src/main/java/com/mia/chatting/adapter/RoomViewHolder.kt
+++ app/src/main/java/com/mia/chatting/adapter/RoomViewHolder.kt
@@ -5,6 +5,10 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.mia.chatting.data.AfterFirebaseData
 import com.mia.chatting.data.FirebaseData
@@ -12,13 +16,17 @@ import com.mia.chatting.data.RoomData
 import com.mia.chatting.data.UserData
 import com.mia.chatting.databinding.LayoutContactItemBinding
 import com.mia.chatting.databinding.LayoutRoomItemBinding
+import com.mia.chatting.util.DataConverter
 import com.mia.chatting.util.DebugLog
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RoomViewHolder(binding: LayoutRoomItemBinding): RecyclerView.ViewHolder(binding.root) {
 
     private val logTag = RoomViewHolder::class.simpleName ?: ""
 
     private val auth: FirebaseAuth
+    private val databaseRef = Firebase.database.reference
 
     private var nameTextView = binding.name
     private var contentTextView = binding.content
@@ -36,21 +44,21 @@ class RoomViewHolder(binding: LayoutRoomItemBinding): RecyclerView.ViewHolder(bi
         DebugLog.i(logTag, "bind-()")
         DebugLog.d(logTag, "afterFirebaseData: $afterFirebaseData")
 
-        changeDataFormat(afterFirebaseData)
+        changeDataFormat(afterFirebaseData) { yourName, after ->
+            nameTextView.text = yourName
+            contentTextView.text = afterFirebaseData.content
+            timeTextView.text = after
 
-        nameTextView.text = afterFirebaseData.outerKey
-        contentTextView.text = afterFirebaseData.content
-        timeTextView.text = afterFirebaseData.sendDate
-
-        val unreadCount = afterFirebaseData.unreadNum.toString()
-        if (unreadCount > 0.toString()) {
-            countTextView.text = afterFirebaseData.unreadNum.toString()
-        } else {
-            countTextView.visibility = View.GONE
+            val unreadCount = afterFirebaseData.unreadNum.toString()
+            if (unreadCount > 0.toString()) {
+                countTextView.text = afterFirebaseData.unreadNum.toString()
+            } else {
+                countTextView.visibility = View.INVISIBLE
+            }
         }
     }
 
-    fun changeDataFormat(origin: AfterFirebaseData) {
+    fun changeDataFormat(origin: AfterFirebaseData, after: (String, String) -> Unit) {
         // uid 파싱해서 변환
         val beforeUid = origin.outerKey
         val beforeStr = beforeUid.split("-")
@@ -63,5 +71,40 @@ class RoomViewHolder(binding: LayoutRoomItemBinding): RecyclerView.ViewHolder(bi
             }
         }
         DebugLog.d(logTag, "counterUid: $counterUid")
+
+
+        var yourName = ""
+
+        val userRef = databaseRef.child("users")
+        userRef.child(counterUid).addListenerForSingleValueEvent(object : ValueEventListener {
+            @SuppressLint("SimpleDateFormat")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                DebugLog.d(logTag, "홀더에서 snapshot => $snapshot")
+                val yourData = DataConverter.jsonToUserData(snapshot)
+                yourName = yourData.name.toString()
+                DebugLog.d(logTag, "홀더에서 yourName => $yourName")
+
+                // 시간 변환
+                val beforeTime = origin.sendDate?.substring(0, 8)
+                val currentDate = SimpleDateFormat("yyyyMMdd").format(Date())
+
+                DebugLog.d(logTag, "beforeTime => $beforeTime")
+                DebugLog.d(logTag, "currentDate => $currentDate")
+
+                beforeTime.let {
+                    if (it == currentDate) {
+                        after.invoke(yourName, "오늘")
+                    } else {
+                        val month = it?.substring(4, 6)
+                        val day = it?.substring(6, 8)
+                        val monthAndDay = "$month/$day"
+                        after.invoke(yourName, monthAndDay)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 }
